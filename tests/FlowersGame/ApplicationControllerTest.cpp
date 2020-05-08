f46126799e9f92104bba476b7aa4339d0ff3c7bf
Mark Oates <markoates0@gmail.com>
2020-05-08 19:48:28 -0400

    Disable FlowersGame/ProgramRunner test due to crash on destruct; al_init() for ApplicationController test

--- tests/FlowersGame/ApplicationControllerTest.cpp
+++ tests/FlowersGame/ApplicationControllerTest.cpp
@@ -10,7 +10,11 @@ TEST(FlowersGame_ApplicationControllerTest, can_be_created_without_blowing_up)
 
 TEST(FlowersGame_ApplicationControllerTest, primary_timer_func__runs_through_the_primary_timer_updates)
 {
+   al_init();
+   ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+
    FlowersGame::ApplicationController application_controller;
    application_controller.primary_timer_func();
-   SUCCEED();
+
+   al_uninstall_system();
 }
