6b9be702257b691f285e5b89bf08c124cd45b63f
Mark Oates <markoates0@gmail.com>
2020-05-08 17:18:50 -0400

    Add AllegroFlare/Framework and all dependencies

---
+++ src/AllegroFlare/Config.cpp
@@ -0,0 +1,117 @@
+
+
+
+#include <AllegroFlare/Config.hpp>
+
+#include <iostream>
+
+
+namespace AllegroFlare
+{
+   Config::Config(std::string filename)
+      : filename(filename)
+      , config_file(nullptr)
+   {
+   }
+
+
+
+   void Config::ensure_initialized_allegro()
+   {
+      if (!al_is_system_installed()) throw std::runtime_error("[Config]: attempting to use AllegroFlare/Config but allegro is not initialized.  You must call al_init() before using any of the AllegroFlare/Config functions.");
+   }
+
+
+
+   bool Config::load()
+   {
+      ensure_initialized_allegro();
+
+      if (config_file) al_destroy_config(config_file);
+      config_file = al_load_config_file(filename.c_str());
+
+      if (!config_file)
+      {
+         std::cerr << "[" << __FUNCTION__ << "] the file \"" << filename << "\" could not be found." << std::endl;
+         return false;
+      }
+      return true;
+   }
+
+
+
+
+   bool Config::has_value(std::string section, std::string key)
+   {
+      ensure_initialized_allegro();
+      const char *val = al_get_config_value(config_file, section.c_str(), key.c_str());
+      if (!val) return false;
+      return true;
+   }
+
+
+
+
+   std::string Config::get_value_str(std::string section, std::string key)
+   {
+      ensure_initialized_allegro();
+      const char *val = al_get_config_value(config_file, section.c_str(), key.c_str());
+      if (!val) return "";
+      return val;
+   }
+
+
+
+
+   int Config::get_value_int(std::string section, std::string key)
+   {
+      ensure_initialized_allegro();
+      const char *val = al_get_config_value(config_file, section.c_str(), key.c_str());
+      if (!val) return 0;
+      return atoi(val);
+   }
+
+
+
+
+   float Config::get_value_float(std::string section, std::string key)
+   {
+      ensure_initialized_allegro();
+      const char *val = al_get_config_value(config_file, section.c_str(), key.c_str());
+      if (!val) return 0;
+      return atof(val);
+   }
+
+
+
+
+   std::string Config::get_or_default_str(std::string section, std::string key, std::string _default)
+   {
+      ensure_initialized_allegro();
+      if (has_value(section, key)) return get_value_str(section, key);
+      return _default;
+   }
+
+
+
+
+   int Config::get_or_default_int(std::string section, std::string key, int _default)
+   {
+      ensure_initialized_allegro();
+      if (has_value(section, key)) return get_value_int(section, key);
+      return _default;
+   }
+
+
+
+
+   float Config::get_or_default_float(std::string section, std::string key, float _default)
+   {
+      ensure_initialized_allegro();
+      if (has_value(section, key)) return get_value_float(section, key);
+      return _default;
+   }
+}
+
+
+
