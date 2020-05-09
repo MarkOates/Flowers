3a6e09ed8b2cbc5d4022cb3d4d91f158f2efab6d
Mark Oates <markoates0@gmail.com>
2020-05-08 21:15:59 -0400

    * Initialize Flowers/GameplayScreen with a new starter flower

--- src/Flowers/GameplayScreen.cpp
+++ src/Flowers/GameplayScreen.cpp
@@ -1,6 +1,8 @@
 
 
 #include <Flowers/GameplayScreen.hpp>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_color.h>
 #include <allegro_flare/placement2d.h>
 #include <Flowers/FlowerRenderer.hpp>
 
@@ -29,6 +31,8 @@ GameplayScreen::~GameplayScreen()
 void GameplayScreen::initialize()
 {
 Flowers::Flower flower;
+flower.set_x(gameboard_width/2);
+flower.set_y(gameboard_height/2);
 flower.set_peduncle_height(40);
 flowers.push_back(flower);
 return;
@@ -37,14 +41,19 @@ return;
 
 void GameplayScreen::primary_timer_func()
 {
-allegro_flare::placement2d place(0, 0, gameboard_width, gameboard_height);
+allegro_flare::placement2d place(800/2, 600/2, gameboard_width, gameboard_height);
 place.align = AllegroFlare::vec2d(0.5, 0.5);
 place.start_transform();
 
+al_draw_rectangle(0, 0, gameboard_width, gameboard_height, al_color_name("red"), 3.0);
+
 for (auto &flower : flowers)
 {
+   allegro_flare::placement2d flower_placement(flower.get_x(), flower.get_y(), 0, 0);
+   flower_placement.start_transform();
    Flowers::FlowerRenderer renderer(&flower);
    renderer.render();
+   flower_placement.restore_transform();
 }
 
 place.restore_transform();
