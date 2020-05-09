56c3900e12262b645bc710198b50b3fa3cfec2c9
Mark Oates <markoates0@gmail.com>
2020-05-09 19:32:51 -0400

    Fix game tests; Remove ApplicationController

--- tests/FlowersGame/GameTest.cpp
+++ tests/FlowersGame/GameTest.cpp
@@ -5,21 +5,24 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 
-TEST(Flowers_GameTest, can_be_created_without_blowing_up)
+TEST(FlowersGame_GameTest, can_be_created_without_blowing_up)
 {
-   Flowers::Game game;
+   FlowersGame::Game game;
 }
 
-TEST(Flowers_GameTest, primary_timer_func__draws_the_title)
+TEST(FlowersGame_GameTest, primary_timer_func__draws_the_title)
 {
    al_init();
    al_init_font_addon();
    al_init_ttf_addon();
 
+   AllegroFlare::Screens screens;
+   AllegroFlare::Framework framework(screens);
+
    ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
    AllegroFlare::FontBin font_bin;
    font_bin.set_full_path("/Users/markoates/Repos/Flowers/bin/programs/data/fonts");
-   Flowers::Game game(&font_bin);
+   FlowersGame::Game game(&framework, &font_bin);
 
    game.primary_timer_func();
    al_flip_display();
