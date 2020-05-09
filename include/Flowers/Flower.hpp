65de357c3cd1f2be76f24edaa3f059d284623d79
Mark Oates <markoates0@gmail.com>
2020-05-08 21:06:05 -0400

    Add "x" and "y" properties to Flower

--- include/Flowers/Flower.hpp
+++ include/Flowers/Flower.hpp
@@ -11,16 +11,22 @@ namespace Flowers
    private:
       ALLEGRO_COLOR color;
       float peduncle_height;
+      float x;
+      float y;
 
    public:
-      Flower(ALLEGRO_COLOR color={}, float peduncle_height=5);
+      Flower(ALLEGRO_COLOR color={}, float peduncle_height=5, float x=0, float y=0);
       ~Flower();
 
       void set_color(ALLEGRO_COLOR color);
       void set_peduncle_height(float peduncle_height);
+      void set_x(float x);
+      void set_y(float y);
 
       ALLEGRO_COLOR get_color();
       float get_peduncle_height();
+      float get_x();
+      float get_y();
    float calculate_receptacle_y();
    };
 }
--- src/Flowers/Flower.cpp
+++ src/Flowers/Flower.cpp
@@ -8,9 +8,11 @@ namespace Flowers
 {
 
 
-Flower::Flower(ALLEGRO_COLOR color, float peduncle_height)
+Flower::Flower(ALLEGRO_COLOR color, float peduncle_height, float x, float y)
    : color(color)
    , peduncle_height(peduncle_height)
+   , x(x)
+   , y(y)
 {
 }
 
@@ -32,6 +34,18 @@ void Flower::set_peduncle_height(float peduncle_height)
 }
 
 
+void Flower::set_x(float x)
+{
+   this->x = x;
+}
+
+
+void Flower::set_y(float y)
+{
+   this->y = y;
+}
+
+
 ALLEGRO_COLOR Flower::get_color()
 {
    return color;
@@ -41,6 +55,18 @@ ALLEGRO_COLOR Flower::get_color()
 float Flower::get_peduncle_height()
 {
    return peduncle_height;
+}
+
+
+float Flower::get_x()
+{
+   return x;
+}
+
+
+float Flower::get_y()
+{
+   return y;
 }
 
 
