7b2b98322a0bf3dc090fbdbb74b6e63654e7af97
Mark Oates <markoates0@gmail.com>
2020-05-09 11:01:13 -0400

    * Update test file

--- tests/Flowers/QuoteGeneratorTest.cpp
+++ tests/Flowers/QuoteGeneratorTest.cpp
@@ -8,13 +8,8 @@ TEST(Flowers_QuoteGeneratorTest, can_be_created_without_blowing_up)
    Flowers::QuoteGenerator quote_generator;
 }
 
-TEST(Flowers_QuoteGeneratorTest, run__returns_the_expected_response)
-{
-}
-
 TEST(Flowers_QuoteGeneratorTest, generate_quote__returns_the_expected_response)
 {
    Flowers::QuoteGenerator quote_generator;
-   std::string expected_string = "Hello World!";
    //EXPECT_EQ(expected_string, quote_generator.generate_quote());
 }
