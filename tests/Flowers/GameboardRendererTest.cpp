a73e77579b99a4780509ee91ad2f1c14cc38ba3a
Mark Oates <markoates0@gmail.com>
2020-05-09 15:10:48 -0400

    * Expand Flowers/GameboardRenderer test

--- tests/Flowers/GameboardRendererTest.cpp
+++ tests/Flowers/GameboardRendererTest.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
 #define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
    try { code; FAIL() << "Expected " # raised_exception_type; } \
    catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
@@ -21,6 +24,20 @@ TEST(Flowers_GameboardRendererTest, render__with_a_nullptr_gameboard_raises_an_e
 
 TEST(Flowers_GameboardRendererTest, render__renders_the_gameboard)
 {
-   Flowers::GameboardRenderer gameboard_renderer;
-   //EXPECT_EQ(expected_string, gameboard_renderer.render());
+   al_init();
+   al_init_primitives_addon();
+
+   ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
+
+   Flowers::Gameboard gameboard;
+   gameboard.spawn_initial_flowers();
+
+   Flowers::GameboardRenderer gameboard_renderer(&gameboard);
+   gameboard_renderer.render();
+
+   al_flip_display();
+
+   sleep(2);
+
+   al_uninstall_system();
 }
