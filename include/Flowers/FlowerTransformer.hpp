28b5fb81f44db118e64fedc007e34b04cf82b1bd
Mark Oates <markoates0@gmail.com>
2020-05-09 23:09:40 -0400

    * Remove #random_int# function, Replace with random

--- include/Flowers/FlowerTransformer.hpp
+++ include/Flowers/FlowerTransformer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <AllegroFlare/Random.hpp>
 #include <Flowers/Flower.hpp>
 #include <allegro5/allegro.h>
 #include <vector>
@@ -12,13 +13,13 @@ namespace Flowers
    {
    private:
       Flowers::Flower* source_flower;
+      AllegroFlare::Random random;
 
    public:
       FlowerTransformer(Flowers::Flower* source_flower=nullptr);
       ~FlowerTransformer();
 
 
-   int random_int_incl(int min=0, int max=1);
    float random_float_incl(int min=0, int max=1);
    ALLEGRO_COLOR random_color();
    Flowers::Flower mutation();
--- src/Flowers/FlowerTransformer.cpp
+++ src/Flowers/FlowerTransformer.cpp
@@ -11,6 +11,7 @@ namespace Flowers
 
 FlowerTransformer::FlowerTransformer(Flowers::Flower* source_flower)
    : source_flower(source_flower)
+   , random({})
 {
 }
 
@@ -19,12 +20,6 @@ FlowerTransformer::~FlowerTransformer()
 {
 }
 
-
-int FlowerTransformer::random_int_incl(int min, int max)
-{
-return rand()%(max-min+1) + min;
-
-}
 
 float FlowerTransformer::random_float_incl(int min, int max)
 {
@@ -35,7 +30,7 @@ return ((float) rand()/RAND_MAX)*(max-min) + min;
 
 ALLEGRO_COLOR FlowerTransformer::random_color()
 {
-return al_map_rgb(random_int_incl(0, 255), random_int_incl(0, 255), random_int_incl(0, 255));
+return al_map_rgb(random.get_random_int(0, 255), random.get_random_int(0, 255), random.get_random_int(0, 255));
 
 }
 
@@ -56,7 +51,7 @@ return flower;
 
 std::vector<Flowers::Flower> FlowerTransformer::mutations()
 {
-int number_of_mutations = random_int_incl(3, 7);
+int number_of_mutations = random.get_random_int(3, 7);
 std::vector<Flowers::Flower> results = {};
 
 for (int i=0; i<number_of_mutations; i++)
